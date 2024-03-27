bcf695bea385630fef6fd38d974d5b04405fea4c
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-03-26 22:16:37 -0300

    melhorando o código e implementando a função de pedir dica

--- Aleatorios/forca.c
+++ Aleatorios/forca.c
@@ -10,6 +10,26 @@
     #define CLEAR "clear"
 #endif
 
+char pedirDica(char letras_palavra[], char letras_acertadas[]) {
+  int letra_aleatoria_invalida;
+  char letra_aleatoria;
+
+  srand(time(NULL));
+  do {
+    letra_aleatoria_invalida = 0;
+    int indice_letra_aleatoria = rand() % (int)strlen(letras_palavra);
+    letra_aleatoria = letras_palavra[indice_letra_aleatoria];
+  
+    for (int i = 0; i < (int)strlen(letras_palavra); i++) {
+      if (tolower(letras_acertadas[i]) == letra_aleatoria) {
+        letra_aleatoria_invalida++;
+      }
+    }
+  } while(letra_aleatoria_invalida != 0);  
+
+  return letra_aleatoria;
+}
+
 int verificarRepetida(char letra, char letras_erradas[], int erros) {
 
   for (int i = 0; i < erros; i++) {
@@ -25,7 +45,7 @@ void verificarLetraNaPalavra(char letra, char letras_palavra[], char letras_acer
   int acertou = 0;
 
   for (int i = 0; i < (int)strlen(letras_palavra); i++) {
-      if (letra == letras_palavra[i]) {
+      if (tolower(letra) == letras_palavra[i]) {
           letras_acertadas[i] = toupper(letra);
           acertou++;
       }
@@ -59,7 +79,7 @@ void printarBonequinho(int erros, char letras_erradas[]) {
   }
 
   printf(">>>>>>> Jogo da Forca <<<<<<<\n\n");
-  
+
   switch(erros) {
     case 0:
       printf(" ______\n |    |        %s\n |\n |\n |\n |\n |             ", letras_erradas_string);
@@ -91,10 +111,21 @@ void printarPalavra(char* palavra_aleatoria, char letras_acertadas[]) {
   }  
 }
 
-char pedirLetra() {
+char pedirLetra(char letras_palavra[], char letras_acertadas[], int *dicas_restantes) {
   char letra;
-  printf("\n\nInsira uma letra: ");
+  printf("\n\n");
+  if (*dicas_restantes > 0) {
+    printf("(insira 0 para receber uma letra da palavra)\n");
+  }
+  printf("Insira uma letra: ");
   scanf(" %c", &letra); 
+
+  if (letra == '0' && *dicas_restantes > 0) {
+    (*dicas_restantes)--;
+    letra = pedirDica(letras_palavra, letras_acertadas);
+  } else if (letra == '0' && *dicas_restantes == 0) {
+    return ' ';
+  }
 
   return letra;
 }
@@ -104,6 +135,7 @@ int main() {
   char* lista_palavras[] = {"casa", "arvore", "carro", "bola", "gato", "cachorro", "livro", "mesa", "caneta", "chuva", "sol", "mar", "vento", "praia", "montanha", "rio", "telefone", "computador", "papel", "copo", "porta", "janela", "cadeira", "corrente", "lampada", "chave", "chuva", "sapato", "estrada", "pedra", "terra", "folha", "flor", "maquina", "fruta", "panela", "lapis", "borracha", "escola", "pessoa", "pote", "garfo", "faca", "prato", "tigela", "garrafa", "fogo", "terra", "arvore", "terra", "fogo", "pedra", "rio", "lago", "ponte", "piscina", "pneu", "cama", "cobertor", "travesseiro", "almofada", "armario", "espelho", "brinquedo", "dinheiro", "banco", "caixa", "cofre", "telefone", "celular", "tablet", "escova", "espuma", "escada", "sabonete", "toalha", "cabelo", "unha", "mao", "barriga", "braco", "perna", "cabeca", "orelha", "nariz", "boca", "vermelho", "verde", "azul", "amarelo", "rosa", "laranja", "roxo", "marrom", "preto", "branco", "cinza", "teclado", "lasanha", "pizza", "carne", "vaca", "galinha", "porco", "banana", "maca", "pessego", "comida", "alimento", "morte", "vida", "felicidade", "tristeza", "amor", "programar", "sorte", "aleatorio", "monitor", "tela", "palavra", "termo", "jogo"};
   srand(time(NULL));
   int indice_aleatorio = rand() % sizeof(lista_palavras)/sizeof(lista_palavras[0]);
+  int dicas_restantes = 1;
   char palavra_aleatoria[12];
   strcpy(palavra_aleatoria, lista_palavras[indice_aleatorio]);
 
@@ -115,10 +147,11 @@ int main() {
     letras_acertadas[i] = '_';
   }
 
+  system(CLEAR);
   while (erros < 6 && strchr(letras_acertadas, '_') != NULL) {
     printarBonequinho(erros, letras_erradas);
     printarPalavra(palavra_aleatoria, letras_acertadas);
-    verificarLetraNaPalavra(pedirLetra(), letras_palavra, letras_acertadas, letras_erradas, erros);
+    verificarLetraNaPalavra(pedirLetra(letras_palavra, letras_acertadas, &dicas_restantes), letras_palavra, letras_acertadas, letras_erradas, erros);
     contarErros(letras_erradas, &erros);
     system(CLEAR);
   }
